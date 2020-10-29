10110db0d942d5db2cce79c258474dc846aa18be
Jay Arredondo <jearredondo13@gmail.com>
2020-10-29 10:40:08 -0500

    building Users page and components

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList'
+
 const Users = () => {
-    return <h2>Users Works!</h2>
+
+    const USERS = [{
+        id: 'u1',
+        name: 'Jay Arredondo', 
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2Fjustjayhimself%2Fstatus%2F677800411382747136&psig=AOvVaw1lSG08zLTjA_j-plFgDk-o&ust=1604072289093000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLi85omR2uwCFQAAAAAdAAAAABAD',
+        places: 3
+    }];
+
+
+    return (
+    <UsersList items={USERS} />
+    );
 }
 
 export default Users;
