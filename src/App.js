ac1a99885ac1cbe644b73a606c20e7f6126c069d
Jay Arredondo <jearredondo13@gmail.com>
2020-10-29 10:19:31 -0500

    installed react router and added new pages to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-function App() {
-  return <h1>Let's start!</h1>;
+import Users from './users/pages/Users';
+import NewPlaces from './places/pages/NewPlaces';
+
+const App = () => {
+  return (
+  <Router>
+    <Switch>
+
+      <Route exact path="/">
+        <Users />
+      </Route>
+      <Route exact path="/newplaces">
+        <NewPlaces />
+      </Route>
+      <Redirect to="/"/>
+      </Switch>
+  </Router>
+  );
 }
 
 export default App;
